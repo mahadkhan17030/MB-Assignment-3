38c99ed828676c42460c8d224a1100ad7e9a8f44
mahadkhan17030 <muhammadmahadkhan80@gmail.com>
2024-05-18 09:39:46 +0500

    work done

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,70 @@
 import React from 'react';
+import { useState }  from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState<any>(0);
+  let [text, setText] = useState<any>("Add Task");
+  let [list, setList] = useState<any>([]);
+
+  const del = (i: any) => {
+    list.splice(i, 1);
+    setList([...list]);
+  };
+  const edit = (i:any, x:any)=>{
+    let a = prompt("Enter edited task", x);
+    list[i] = a;
+    setList([...list]);
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        {}
+
+        <div>
+          <p>{text}</p>
+          <input
+            type="text"
+            onChange={(e) => {
+              setText(e.target.value);
+            }}
+          />
+          <button
+            className=""
+            onClick={() => {
+              list.push(text);
+              setList([...list]);
+            }}
+          >
+            Add Task
+          </button>
+
+          {list.map((x: any, i: any) => (
+            <>
+              <p key={i}>{x}</p>
+              <button
+                type="button"
+                onClick={() => {
+                  del(i);
+                }}
+              >
+                Delete
+              </button>
+              <button
+                onClick={() => {
+                  edit(i, x);
+                }}
+              >
+                Change
+              </button>
+            </>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
 
